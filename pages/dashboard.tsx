d7c47f52930dc451f0a923d94c4259842c82e510
vik <vik@Samuels-MBP-2.attlocal.net>
2024-09-12 10:00:31 -0500

    chore: bg

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -32,10 +32,10 @@ export default function Dashboard() {
             <Separator />
           </div>
           <main
-            className="flex flex-col gap-4 relative grow overflow-hidden overflow-y-scroll bg-stone-200/50 shadow-inner"
+            className="flex flex-col gap-4 relative grow overflow-hidden overflow-y-scroll bg-stone-200 shadow-inner"
             style={{
               backgroundImage:
-                "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='lightgray' fill-opacity='0.25'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
+                "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='gray' fill-opacity='0.10'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
             }}
           >
             <MobileMenuSheet
