d792eccbfb4adfcd8d59607206ae413f2e8e10be
vik <vik@Samuels-MBP-2.attlocal.net>
2024-09-15 11:39:16 -0500

    chore: fix two day programming

--- lib/programming/two_day.ts
+++ lib/programming/two_day.ts
@@ -1,6 +1,8 @@
 import {
   AUX_UTILITY,
+  BACK_GENERAL,
   BACK_LAT,
+  BASIC_UTILITY,
   CHEST_GENERAL,
   CHEST_UPPER,
   EXCLUDE_ERECTOR_SPINAE,
@@ -21,7 +23,11 @@ export const TWO_DAY_SPLIT_PROGRAMMING: Splits = {
   [TWO_DAY_MUSCLES.TLA]: [
     [
       { target: "BACK", optional: false, filters: [BACK_LAT] },
-      { target: "BACK", optional: false, filters: [EXCLUDE_ERECTOR_SPINAE] },
+      {
+        target: "BACK",
+        optional: false,
+        filters: [EXCLUDE_ERECTOR_SPINAE, BACK_GENERAL],
+      },
       { target: "CHEST", optional: false, filters: [CHEST_GENERAL] },
       { target: "CHEST", optional: true, filters: [CHEST_UPPER] },
       { target: "SHOULDERS", optional: false, filters: [SHOULDER_LATERAL] },
@@ -36,12 +42,12 @@ export const TWO_DAY_SPLIT_PROGRAMMING: Splits = {
       {
         target: "HAMSTRINGS",
         optional: false,
-        filters: [EXCLUDE_ERECTOR_SPINAE, AUX_UTILITY],
+        filters: [EXCLUDE_ERECTOR_SPINAE],
       },
-      { target: "TRICEPS", optional: false },
-      { target: "BICEPS", optional: false },
-      { target: "CALVES", optional: false },
-      { target: "ABS", optional: false },
+      { target: "TRICEPS", optional: false, filters: [BASIC_UTILITY] },
+      { target: "BICEPS", optional: false, filters: [BASIC_UTILITY] },
+      { target: "CALVES", optional: false, filters: [BASIC_UTILITY] },
+      { target: "ABS", optional: false, filters: [BASIC_UTILITY] },
     ],
   ],
   [TWO_DAY_MUSCLES.PP]: [
@@ -52,14 +58,22 @@ export const TWO_DAY_SPLIT_PROGRAMMING: Splits = {
         optional: true,
         filters: [INCLUDE_ERECTOR_SPINAE, AUX_UTILITY],
       },
-      { target: "CHEST", optional: false, filters: [CHEST_GENERAL] },
+      {
+        target: "CHEST",
+        optional: false,
+        filters: [CHEST_GENERAL, BASIC_UTILITY],
+      },
       { target: "CHEST", optional: true, filters: [CHEST_UPPER] },
-      { target: "TRICEPS", optional: true },
-      { target: "CALVES", optional: false },
+      { target: "TRICEPS", optional: true, filters: [BASIC_UTILITY] },
+      { target: "CALVES", optional: false, filters: [BASIC_UTILITY] },
     ],
     [
       { target: "BACK", optional: false, filters: [BACK_LAT] },
-      { target: "BACK", optional: false, filters: [EXCLUDE_ERECTOR_SPINAE] },
+      {
+        target: "BACK",
+        optional: false,
+        filters: [EXCLUDE_ERECTOR_SPINAE, BACK_GENERAL],
+      },
       { target: "SHOULDER", optional: false, filters: [SHOULDER_LATERAL] },
       { target: "BICEPS", optional: false },
       {
@@ -67,19 +81,25 @@ export const TWO_DAY_SPLIT_PROGRAMMING: Splits = {
         optional: false,
         filters: [EXCLUDE_ERECTOR_SPINAE],
       },
-      { target: "ADDUCTORS", optional: true },
-      { target: "ABDUCTORS", optional: true },
       { target: "ABS", optional: false },
     ],
   ],
   [TWO_DAY_MUSCLES.UL]: [
     [
       { target: "BACK", optional: false, filters: [BACK_LAT] },
-      { target: "BACK", optional: false, filters: [EXCLUDE_ERECTOR_SPINAE] },
-      { target: "CHEST", optional: false, filters: [CHEST_GENERAL] },
+      {
+        target: "BACK",
+        optional: false,
+        filters: [EXCLUDE_ERECTOR_SPINAE, BACK_GENERAL],
+      },
+      {
+        target: "CHEST",
+        optional: false,
+        filters: [CHEST_GENERAL, BASIC_UTILITY],
+      },
       { target: "SHOULDERS", optional: false, filters: [SHOULDER_LATERAL] },
-      { target: "TRICEPS", optional: true },
-      { target: "BICEPS", optional: true },
+      { target: "TRICEPS", optional: true, filters: [BASIC_UTILITY] },
+      { target: "BICEPS", optional: true, filters: [BASIC_UTILITY] },
     ],
     [
       { target: "QUADS", optional: false, filters: [INCLUDE_ERECTOR_SPINAE] },
