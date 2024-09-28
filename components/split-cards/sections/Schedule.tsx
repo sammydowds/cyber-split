4a14b4dba6e72dc79c7fe4c83f3ed13f41ded50a
vik <sammydowds1993@gmail.com>
2024-09-28 08:58:31 -0500

    chore: add schedule description

--- components/split-cards/sections/Schedule.tsx
+++ components/split-cards/sections/Schedule.tsx
@@ -1,6 +1,7 @@
 import { SplitDeep } from "@/types";
 import { Day } from "@/lib/programming/createWorkoutSchedule";
 import { MonthTable } from "../../MonthTable";
+import { CADENCE_TO_DESCRIPTION_MAP } from "@/lib/programming/constants";
 
 const DAYS = [
   "Sundays",
@@ -20,13 +21,16 @@ export const Schedule = ({
   split: SplitDeep;
 }) => {
   const skipDayText = split.skipDays?.length
-    ? `This schedule skips ${split.skipDays.map((d) => DAYS[d]).join(", ")}.`
+    ? `This schedule skips ${split.skipDays.map((d) => DAYS[d]).join(", ")}`
     : undefined;
   return (
     <div className="text-sm flex flex-col px-4">
       <div className="font-bold">Schedule</div>
       <div className="flex items-center gap-[4px] text-xs text-stone-500">
-        <div>{skipDayText ?? "No days are skipped for this schedule."}</div>
+        <div>
+          {CADENCE_TO_DESCRIPTION_MAP[split.type][split.cadence]} •{" "}
+          {skipDayText ?? "No days skipped"}
+        </div>
       </div>
       <MonthTable weeks={weeks} />
     </div>
