7f29864008088778af22f4d6f8738a6779df27a9
vik <sammydowds1993@gmail.com>
2024-10-22 10:02:07 -0500

    chore: remove unused service

--- services/daily-workout/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@repo/database";
-import assert from "assert";
-
-const textUsers = async () => {};
-// get user info
-// get user active split
-// get user workout today
-// if user phone and notifications enabled and workout today
-// send text
