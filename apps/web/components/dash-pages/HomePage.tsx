4104ee40b26479887f5437d6967566ae192cf385
vik <sammydowds1993@gmail.com>
2024-11-16 08:56:54 -0600

    chore: remove old component and wire up home page

--- apps/web/components/dash-pages/HomePage.tsx
+++ apps/web/components/dash-pages/HomePage.tsx
@@ -2,9 +2,9 @@ import { ActiveSplitDeep } from "@repo/database";
 import { UserStats } from "./components/UserStats";
 import { SplitUpcomingWorkouts } from "./components/SplitUpcomingWorkouts";
 import { WeeklyVolumeBarChart } from "../WeeklyVolumeBarChart";
-import { Discover } from "./components/Discover";
 import { SplitCarousel } from "../SplitCarousel";
 import { useDiscoverSplits } from "@/hooks/useDiscoverSplits";
+import { Loading } from "../Loading";
 
 interface HomePageProps {
   activeSplit?: ActiveSplitDeep;
@@ -15,13 +15,17 @@ export const HomePage = ({ activeSplit }: HomePageProps) => {
     <div className="pt-8 flex justify-center">
       <div className="flex flex-col items-center w-full gap-6 text-black">
         <div className="w-full bg-yellow-300 py-4 flex items-center">
-          <Discover />
+          {isPending || isRefetching ? (
+            <div className="min-h-[460px] flex items-center justify-center w-full">
+              <Loading />
+            </div>
+          ) : (
+            <SplitCarousel splits={data ?? []} />
+          )}
         </div>
         <UserStats activeSplit={activeSplit} />
         <WeeklyVolumeBarChart />
-        <div className="w-full bg-yellow-300 py-4 flex items-center">
-          <SplitCarousel splits={data ?? []} />
-        </div>
+
         <SplitUpcomingWorkouts activeSplit={activeSplit} />
       </div>
     </div>
