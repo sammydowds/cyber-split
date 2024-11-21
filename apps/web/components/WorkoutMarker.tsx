cff349133190c0a63ef92b4e870862933d84ee3d
vik <sammydowds1993@gmail.com>
2024-11-21 16:59:56 -0600

    chore: workout marker sizes

--- apps/web/components/WorkoutMarker.tsx
+++ apps/web/components/WorkoutMarker.tsx
@@ -23,7 +23,7 @@ export const WorkoutMarker = ({ text, className }: WorkoutMarkerProps) => {
   return (
     <div
       className={cn(
-        "flex items-center justify-center h-7 w-7 rounded text-black border-[1px] border-stone-500 font-extrabold text-[14px] shadow-[2px_2px_2px_rgba(0,0,0,0.15)]",
+        "flex items-center justify-center h-7 w-7 rounded text-black border-[1px] border-stone-500 font-extrabold text-[14px]",
         className,
         getBgColor(text),
       )}
